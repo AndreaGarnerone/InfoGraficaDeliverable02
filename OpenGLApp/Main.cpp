0a827bfea19b6e39ec82f23adb82c0c59840365e
AndreaGarnerone <aa.garnerone@gmail.com>
2024-12-22 19:07:10 +0100

    Added 3D models

--- OpenGLApp/Main.cpp
+++ OpenGLApp/Main.cpp
@@ -1,6 +1,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <random>
+#include <vector>
+#include <string>
 #include "stb_image.h"
 
 #include <glm/glm.hpp>
@@ -13,11 +15,16 @@
 #include <iostream>
 #include <map>
 
+
 #include <ft2build.h>
 #include FT_FREETYPE_H
 
 #include <irrKlang.h>
 using namespace irrklang;
+
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
 
 // Collision handling
 	// AABB (Axis-Aligned Bounding Box) structure
@@ -35,17 +42,6 @@ struct Character {
 
 std::map<char, Character> Characters;
 unsigned int txtVAO, txtVBO;
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void mouse_callback(GLFWwindow* window, double xpos, double ypos);
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
-void processInput(GLFWwindow* window);
-glm::vec3 generateRandomPosition();
-AABB createAABB(const glm::vec3& position);
-bool checkCollision(const AABB& a, const AABB& b);
-void renderText(Shader& s, std::string text, float x, float y, float scale, glm::vec3 color);
-void initFreetype(const char* fontPath);
-
 
 // settings
 const unsigned int SCR_WIDTH = 800;
@@ -76,9 +72,192 @@ glm::vec3 platePosition = { 0.0f, -1.10f, 0.0f };
 
 ISoundEngine* soundEngine = createIrrKlangDevice();
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow* window);
+glm::vec3 generateRandomPosition();
+AABB createAABB(const glm::vec3& position);
+bool checkCollision(const AABB& a, const AABB& b);
+void renderText(Shader& s, std::string text, float x, float y, float scale, glm::vec3 color);
+unsigned int TextureFromFile(const char* path, const std::string& directory);
+
+// Struct for Vertex
+struct Vertex {
+	glm::vec3 Position;
+	glm::vec3 Normal;
+	glm::vec2 TexCoords;
+};
+
+// Struct for Texture
+struct Texture {
+	unsigned int id;
+	std::string type;
+	std::string path;
+};
+
+// Mesh class
+class Mesh {
+public:
+	std::vector<Vertex> vertices;
+	std::vector<unsigned int> indices;
+	std::vector<Texture> textures;
+	unsigned int VAO;
+
+	Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<Texture> textures) {
+		this->vertices = vertices;
+		this->indices = indices;
+		this->textures = textures;
+		setupMesh();
+	}
+
+	void Draw(Shader& shader) {
+		unsigned int diffuseNr = 1;
+		unsigned int specularNr = 1;
+		for (unsigned int i = 0; i < textures.size(); i++) {
+			glActiveTexture(GL_TEXTURE0 + i);
+			std::string number;
+			std::string name = textures[i].type;
+			if (name == "texture_diffuse")
+				number = std::to_string(diffuseNr++);
+			else if (name == "texture_specular")
+				number = std::to_string(specularNr++);
+			shader.setInt(("material." + name + number).c_str(), i);
+			glBindTexture(GL_TEXTURE_2D, textures[i].id);
+		}
+		glActiveTexture(GL_TEXTURE0);
+
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+	}
+
+private:
+	unsigned int VBO, EBO;
+
+	void setupMesh() {
+		glGenVertexArrays(1, &VAO);
+		glGenBuffers(1, &VBO);
+		glGenBuffers(1, &EBO);
+
+		glBindVertexArray(VAO);
+
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
+
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
+		glEnableVertexAttribArray(2);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
+
+		glBindVertexArray(0);
+	}
+};
+
+// Model class
+class Model {
+public:
+	Model(const std::string& path) { loadModel(path); }
+
+	void Draw(Shader& shader) {
+		for (unsigned int i = 0; i < meshes.size(); i++)
+			meshes[i].Draw(shader);
+	}
+
+private:
+	std::vector<Mesh> meshes;
+	std::string directory;
+
+	void loadModel(const std::string& path) {
+		Assimp::Importer importer;
+		const aiScene* scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_GenSmoothNormals);
+		if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) {
+			std::cerr << "ERROR::ASSIMP:: " << importer.GetErrorString() << std::endl;
+			return;
+		}
+		directory = path.substr(0, path.find_last_of('/'));
+		processNode(scene->mRootNode, scene);
+	}
+
+	void processNode(aiNode* node, const aiScene* scene) {
+		for (unsigned int i = 0; i < node->mNumMeshes; i++) {
+			aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
+			meshes.push_back(processMesh(mesh, scene));
+		}
+		for (unsigned int i = 0; i < node->mNumChildren; i++) {
+			processNode(node->mChildren[i], scene);
+		}
+	}
+
+	Mesh processMesh(aiMesh* mesh, const aiScene* scene) {
+		std::vector<Vertex> vertices;
+		std::vector<unsigned int> indices;
+		std::vector<Texture> textures;
+
+		for (unsigned int i = 0; i < mesh->mNumVertices; i++) {
+			Vertex vertex;
+			glm::vec3 vector;
+			vector.x = mesh->mVertices[i].x;
+			vector.y = mesh->mVertices[i].y;
+			vector.z = mesh->mVertices[i].z;
+			vertex.Position = vector;
+
+			vector.x = mesh->mNormals[i].x;
+			vector.y = mesh->mNormals[i].y;
+			vector.z = mesh->mNormals[i].z;
+			vertex.Normal = vector;
+
+			if (mesh->mTextureCoords[0]) {
+				glm::vec2 vec;
+				vec.x = mesh->mTextureCoords[0][i].x;
+				vec.y = mesh->mTextureCoords[0][i].y;
+				vertex.TexCoords = vec;
+			}
+			else
+				vertex.TexCoords = glm::vec2(0.0f, 0.0f);
+
+			vertices.push_back(vertex);
+		}
+
+		for (unsigned int i = 0; i < mesh->mNumFaces; i++) {
+			aiFace face = mesh->mFaces[i];
+			for (unsigned int j = 0; j < face.mNumIndices; j++)
+				indices.push_back(face.mIndices[j]);
+		}
+
+		if (mesh->mMaterialIndex >= 0) {
+			aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
+			std::vector<Texture> diffuseMaps = loadMaterialTextures(material, aiTextureType_DIFFUSE, "texture_diffuse");
+			textures.insert(textures.end(), diffuseMaps.begin(), diffuseMaps.end());
+			std::vector<Texture> specularMaps = loadMaterialTextures(material, aiTextureType_SPECULAR, "texture_specular");
+			textures.insert(textures.end(), specularMaps.begin(), specularMaps.end());
+		}
+
+		return Mesh(vertices, indices, textures);
+	}
+
+	std::vector<Texture> loadMaterialTextures(aiMaterial* mat, aiTextureType type, const std::string& typeName) {
+		std::vector<Texture> textures;
+		for (unsigned int i = 0; i < mat->GetTextureCount(type); i++) {
+			aiString str;
+			mat->GetTexture(type, i, &str);
+			Texture texture;
+			texture.id = TextureFromFile(str.C_Str(), directory);
+			texture.type = typeName;
+			texture.path = str.C_Str();
+			textures.push_back(texture);
+		}
+		return textures;
+	}
+};
+
 int main()
 {
-
 	// glfw: initialize and configure
 	// ------------------------------
 	glfwInit();
@@ -122,6 +301,10 @@ int main()
 	// build and compile our shader zprogram
 	// ------------------------------------
 	Shader ourShader("shader.vs", "shader.fs");
+
+	Model croissantModel("C:/Users/aagar/Documents/InfoGrafica/OpenGLApp  - demos/OpenGLApp/OpenGLApp/croissant.obj");
+	Model plateModel("C:/Users/aagar/Documents/InfoGrafica/OpenGLApp  - demos/OpenGLApp/OpenGLApp/sgorbio.obj");
+	Model otherModel("C:/Users/aagar/Documents/InfoGrafica/OpenGLApp  - demos/OpenGLApp/OpenGLApp/togocup.obj");
 
 	// Text handling
 	// --------------------------------------
@@ -230,8 +413,8 @@ int main()
 		-0.60f, 1.20f, -0.01f,   0.0f, 1.0f   // top left
 	};
 	glm::vec3 conveyorBeltPositions[] = {
-	 glm::vec3(0.0f, 0.0f, 0.0f),
-	 glm::vec3(0.0f, 2.4f, 0.0f)
+	glm::vec3(0.0f, 0.0f, 0.0f),
+	glm::vec3(0.0f, 2.4f, 0.0f)
 	};
 
 	float conveyorSpeed = 0.002f; // Speed of scrolling
@@ -478,11 +661,27 @@ int main()
 				soundEngine->play2D("C:/Users/aagar/Documents/InfoGrafica/OpenGLApp  - demos/OpenGLApp/OpenGLApp/pickup_sound.wav", false);
 			}
 
-
 			// Render cube
 			glm::mat4 objModel = glm::mat4(1.0f);
-			objModel = glm::translate(objModel, objectsPositions[i]);
-			ourShader.setMat4("model", objModel);
+			if (i % 2 == 0) {
+				float angle = glfwGetTime(); // Use the current time as the angle in radians
+				objModel = glm::translate(objModel, objectsPositions[i]);
+				objModel = glm::scale(objModel, glm::vec3(0.3f, 0.3f, 0.3f));
+				objModel = glm::rotate(objModel, angle, glm::vec3(0.0f, 1.0f, 0.0f));
+
+				//ourShader.setInt("textureID", 1);	// still show the croissant white
+				ourShader.setMat4("model", objModel);
+				croissantModel.Draw(ourShader);
+			} else {
+				float angle = glfwGetTime(); // Use the current time as the angle in radians
+				objModel = glm::translate(objModel, objectsPositions[i]);
+				objModel = glm::scale(objModel, glm::vec3(0.1f, 0.1f, 0.1f));
+				objModel = glm::rotate(objModel, angle, glm::vec3(0.0f, 1.0f, 0.0f));
+
+				//ourShader.setInt("textureID", 1);	// still show the croissant white
+				ourShader.setMat4("model", objModel);
+				otherModel.Draw(ourShader);
+			}
 			glBindVertexArray(VAO);
 			glDrawArrays(GL_TRIANGLES, 0, 6);
 		}
@@ -536,15 +735,24 @@ int main()
 		glBindTexture(GL_TEXTURE_2D, texture1);
 		glBindVertexArray(plateVAO);
 		model = glm::translate(glm::mat4(1.0f), platePosition);
+		model = glm::scale(model, glm::vec3(0.15f, 0.15f, 0.15f));
 		ourShader.setMat4("model", model);
 		ourShader.setInt("textureID", 1);
+		plateModel.Draw(ourShader);
 		glDrawArrays(GL_TRIANGLES, 0, 6);
 
+		// Render cube
+		/*
+		glm::mat4 objModel = glm::mat4(1.0f);
+		ourShader.setMat4("model", objModel);
+		*/
+
 		// render cubes
+		/*
 		glBindVertexArray(plateVAO);
 		glBindVertexArray(conveyorVAO);
-		ourShader.setInt("textureID", 2);
 		glBindVertexArray(VAO);
+		*/
 
 		// Render text
 		glUseProgram(shader.ID); // Use text shader
@@ -581,6 +789,40 @@ int main()
 	// ------------------------------------------------------------------
 	glfwTerminate();
 	return 0;
+}
+
+// Helper function for loading textures
+unsigned int TextureFromFile(const char* path, const std::string& directory) {
+	std::string filename = directory + "/" + std::string(path);
+	unsigned int textureID;
+	glGenTextures(1, &textureID);
+	int width, height, nrComponents;
+	unsigned char* data = stbi_load(filename.c_str(), &width, &height, &nrComponents, 0);
+	if (data) {
+		GLenum format;
+		if (nrComponents == 1)
+			format = GL_RED;
+		else if (nrComponents == 3)
+			format = GL_RGB;
+		else if (nrComponents == 4)
+			format = GL_RGBA;
+
+		glBindTexture(GL_TEXTURE_2D, textureID);
+		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		stbi_image_free(data);
+	}
+	else {
+		std::cerr << "Texture failed to load at path: " << path << std::endl;
+		stbi_image_free(data);
+	}
+	return textureID;
 }
 
 glm::vec3 generateRandomPosition() {
